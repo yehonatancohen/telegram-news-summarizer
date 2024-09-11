ff2a7f3e92b2096055d38f66d8faf25864558ed6
yehonatancohen <yoncohenyon@gmail.com>
2024-09-11 20:45:13 +0300

    Removed unnessecary logs

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -4,6 +4,8 @@ import os
 
 # Load the logging configuration
 LOG_CONFIG_PATH = os.path.join(os.path.dirname(__file__), '../../config/logging.conf')
+logging.getLogger('telethon.crypto.libssl').setLevel(logging.CRITICAL)
+logging.getLogger('telethon.crypto.aes').setLevel(logging.CRITICAL)
 logging.config.fileConfig(LOG_CONFIG_PATH)
 
 # Create a logger that can be imported in other modules
