757476742a76c8fb3aefdf19fe7301f3c23ec252
yehonatancohen <yoncohenyon@gmail.com>
2024-09-12 00:32:59 +0300

    Fixed async and logger problems

--- src/main.py
+++ src/main.py
@@ -7,23 +7,26 @@ sys.path.append(parent)
 from config import get_env_variable
 
 async def main():
+    logger.disabled = False
     logger.info("Starting tasks...")
 
     # Get the API ID and API hash from the environment variables
     api_id = get_env_variable("API_ID")
     api_hash = get_env_variable("API_HASH")
+    phone_number = get_env_variable("PHONE_NUMBER")
+    password = get_env_variable("PASSWORD")
 
     # Create a Telegram client and connect to the Telegram API
     telegram = Telegram(api_id=api_id, api_hash=api_hash)
-    telegram.connect()
+    await telegram.connect(phone_number, password)
 
     # Join channels
     channel_ids = get_env_variable("CHANNEL_IDS").split(",")
     for channel_id in channel_ids:
-        telegram.join_channel(channel_id)
+        await telegram.join_channel(channel_id)
 
     # Listen to incoming messages
-    telegram.listen_messages()
+    await telegram.listen_messages()
     
 if __name__ == "__main__":
     asyncio.run(main())
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,11 +2,25 @@ import logging
 import logging.config
 import os
 
-# Load the logging configuration
-LOG_CONFIG_PATH = os.path.join(os.path.dirname(__file__), '../../config/logging.conf')
-logging.getLogger('telethon.crypto.libssl').setLevel(logging.CRITICAL)
-logging.getLogger('telethon.crypto.aes').setLevel(logging.CRITICAL)
-logging.config.fileConfig(LOG_CONFIG_PATH)
+logger = None
+
+def init_logger():
+    global logger
+    if (logger):
+        return logger
+    # Load the logging configuration
+    LOG_CONFIG_PATH = os.path.join(os.path.dirname(__file__), '../../config/logging.conf')
+    logging.getLogger('telethon.crypto.libssl').setLevel(logging.CRITICAL)
+    logging.getLogger('telethon.crypto.aes').setLevel(logging.CRITICAL)
+    logging.config.fileConfig(LOG_CONFIG_PATH)
+
+    # Initialize the logger
+    _logger = logging.getLogger(__name__)
+    
+    # Logger disabled by default
+    _logger.disabled = False
+
+    return _logger
 
 # Create a logger that can be imported in other modules
-logger = logging.getLogger(__name__)
+logger = init_logger()
