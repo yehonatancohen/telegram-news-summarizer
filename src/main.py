6755f592f6dc58690e3e503669f0f009bcd759e9
Yehonatan Cohen <coheyeho@gmail.com>
2024-09-21 17:25:01 +0300

    Added channels

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from utils import logger
 from telegram import Telegram
+from db import init_channels, add_telegram_channel, get_telegram_channels
 import asyncio, sys, os
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -20,10 +21,55 @@ async def main():
     telegram = Telegram(api_id=api_id, api_hash=api_hash)
     await telegram.connect(phone_number, password)
 
+    # Initialize the database
+    init_channels()
+
+    arab_channels = [
+        '@a7rarjenin',
+        '@QudsN',
+        '@Electrohizbullah',
+        '@SerajSat',
+        '@shadysopoh',
+        '@jeninqassam',
+        '@Janin324',
+        '@jenin4',
+        '@anas_hoshia',
+        '@abohamzahasanat',
+        '@sarayajneen',
+        '@abohamzahasanat',
+        '@C_Military1',
+        '@mmirleb',
+        '@SabrenNews22',
+        '@IraninArabic',
+        '@iraninarabic_ir',
+        '@meshheek',
+        '@qassam1brigades',
+        '@qassambrigades',
+        '@duyuf1',
+        '@Ail_2_9',
+        '@alghalebun3',
+        '@areennabluss'
+    ]
+
+    smart_channels = [
+        '@abualiexpress',
+        '@arabworld301',
+        '@AlealamAlearabiuEranMalca',
+        '@AsrarLubnan'
+    ]
+
+    for channel in arab_channels:
+        add_telegram_channel(channel, 'Arab', 1)
+    for channel in smart_channels:
+        add_telegram_channel(channel, 'Smart', 2)
+
+    channels = get_telegram_channels()
+    print(channels)
+
     # Join channels
-    channel_ids = get_env_variable("CHANNEL_IDS").split(",")
-    for channel_id in channel_ids:
-        await telegram.join_channel(channel_id)
+    #channel_ids = get_env_variable("CHANNEL_IDS").split(",")
+    for channel in channels:
+        await telegram.join_channel(channel['id'])
 
     # Listen to incoming messages
     await telegram.listen_messages()
